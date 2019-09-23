ba245d25372e4cd062ad4b05de1ed3a98654eac6
cambridgejames <cambridge_james@foxmail.com>
2019-09-23 17:55:12 +0800

    完成了列表的基本结构和样式

--- core/config/website.php
+++ core/config/website.php
@@ -3,18 +3,18 @@
 return array(
 	'list' => array(
 		'itexam.compscosys.cn' => array(
-			'icon' => 'http://itexam.compscosys.cn/favicon.ico',
+			'icon' => '/static/img/itexam.ico',
 			'title' => 'ITEXAM网上作业系统',
 			'view' => '一个网上作业系统'
 		),
 		'coss.compscosys.cn' => array(
-			'icon' => 'http://coss.compscosys.cn/favicon.ico',
+			'icon' => '/static/img/coss.ico',
 			'title' => '竞赛在线评分系统',
 			'view' => '一个竞赛在线评分系统，可供用户在线创建竞赛并设置裁判、评分标准、是否公开等内容，并可以在游客端或总览中实时显示各个参赛队伍的评分、排名等信息。此外，还可以通过成绩导出功能下载最终评分的数据表格等作为保存'
 		),
 		'quickbi.compscosys.cn' => array(
-			'icon' => 'http://quickbi.compscosys.cn/favicon.ico',
-			'title' => '学生信息数据可视化展示',
+			'icon' => '/static/img/quickbi.ico',
+			'title' => '学生数据可视化展示',
 			'view' => '一个学生信息数据可视化展示页面'
 		)
 	)

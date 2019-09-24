f9586780ccaa57e612b535d1d75aad2adad78c71
cambridgejames <cambridge_james@foxmail.com>
2019-09-24 23:38:29 +0800

    更新了前端样式和鼠标点击跳转功能
    1. 修改了站点目录列表；
    2. 增加了对https协议的支持；
    3. 修复了工信部链接不能正确访问的问题；
    4. 将站点图标更改为矢量图标；
    5. 增加了鼠标点击跳转功能；
    6. 修复了在非Webkit内核的浏览器中站点标签的背景不能正常显示的问题

--- core/config/website.php
+++ core/config/website.php
@@ -3,24 +3,40 @@
 return array(
 	'list' => array(
 		'www.compscosys.cn' => array(
-			'icon' => '/favicon.ico',
+			'icon' => '/static/img/erro.svg',
+			'method' => 'http',
 			'title' => '站点目录管理器',
 			'view' => '用于管理多个虚拟网站，提供网站的入口；同时可用于自定义错误代码（如404、500等）的显示'
 		),
 		'itexam.compscosys.cn' => array(
-			'icon' => '/static/img/itexam.ico',
+			'icon' => '/static/img/itexam.svg',
+			'method' => 'http',
 			'title' => 'ITEXAM网上作业系统',
 			'view' => '一个网上作业系统'
 		),
 		'coss.compscosys.cn' => array(
-			'icon' => '/static/img/coss.ico',
+			'icon' => '/static/img/coss.svg',
+			'method' => 'http',
 			'title' => '竞赛在线评分系统',
 			'view' => '一个竞赛在线评分系统，可供用户在线创建竞赛并设置裁判、评分标准、是否公开等内容，并可以在游客端或总览中实时显示各个参赛队伍的评分、排名等信息。此外，还可以通过成绩导出功能下载最终评分的数据表格等作为保存'
 		),
 		'quickbi.compscosys.cn' => array(
-			'icon' => '/static/img/quickbi.ico',
+			'icon' => '/static/img/quickbi.svg',
+			'method' => 'http',
 			'title' => '学生数据可视化展示',
 			'view' => '一个学生信息数据可视化展示页面'
+		),
+		'gitblit.compscosys.cn:8080' => array(
+			'icon' => '/static/img/gitblit.svg',
+			'method' => 'http',
+			'title' => 'Gitblit私有git服务',
+			'view' => "Gitblit is an open-source, pure Java stack for managing, viewing, and serving Git repositories. It's designed primarily as a tool for small workgroups who want to host centralized repositories."
+		),
+		'github.com/cambridgejames' => array(
+			'icon' => '/static/img/github.svg',
+			'method' => 'https',
+			'title' => '个人GitHub存储库',
+			'view' => "一个面向开源及私有软件项目的托管平台"
 		)
 	)
 );
